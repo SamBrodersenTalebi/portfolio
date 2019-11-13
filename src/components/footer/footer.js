3b178254db63fc9968ad09359b31d56ab49e506f
Sam Brodersen <sam.talebi84@gmail.com>
2019-11-13 09:44:58 +0100

    initial commit

---
+++ src/components/footer/footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './footer.css';
+
+export default class AboutMe extends React.Component{
+    render(){
+        return(
+            <div className="footer" id="Contact">  
+                <p> © 2019. All Rights Reserved, designed by Sam Brodersen</p>
+            </div>
+        );
+    }
+}
---
+++ src/components/header/header.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import  "./header.css"
+
+export default class Header extends Component{
+
+  render(){
+
+    return(
+      <header className="header" id="Home">
+        <h1>Hi, I'm <span className="name"> Sam Brodersen</span>.</h1>
+        <p> I'm a front-end developer currently studying at OpenClassroooms </p>
+        <a href="#Projects">My Projects</a>
+      </header>
+    );
+  }
+}
