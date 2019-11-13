bb1c9a260e3703267ddc04362e17eb014cbc8a2c
Sam Brodersen <sam.talebi84@gmail.com>
2019-11-13 09:53:32 +0100

    Updates

---
+++ precache-manifest.6da68b87cc0976ff33321e37d46298ed.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58be96dbaac75e82138e2d65b5d169a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "370318e74ee7391370f9",
+    "url": "/portfolio/static/css/2.559fd73f.chunk.css"
+  },
+  {
+    "revision": "be1a3a87d7d1bb2f902d",
+    "url": "/portfolio/static/css/main.a3495855.chunk.css"
+  },
+  {
+    "revision": "370318e74ee7391370f9",
+    "url": "/portfolio/static/js/2.589fc118.chunk.js"
+  },
+  {
+    "revision": "be1a3a87d7d1bb2f902d",
+    "url": "/portfolio/static/js/main.935ef5b8.chunk.js"
+  },
+  {
+    "revision": "1bec89c3e591b3df5266",
+    "url": "/portfolio/static/js/runtime-main.5f9b180c.js"
+  },
+  {
+    "revision": "5a65087bd6be04582bab3ce76ab274b9",
+    "url": "/portfolio/static/media/BoardGame.5a65087b.PNG"
+  },
+  {
+    "revision": "ef00982fa0572a56a91913ab37b39174",
+    "url": "/portfolio/static/media/Brooklyn.ef00982f.PNG"
+  },
+  {
+    "revision": "09c9c77cbd11670ce778f399d505f252",
+    "url": "/portfolio/static/media/Jammming.09c9c77c.PNG"
+  },
+  {
+    "revision": "71bb96de8b194cbbe9a74c8db66753af",
+    "url": "/portfolio/static/media/PixelArt.71bb96de.PNG"
+  },
+  {
+    "revision": "90affcd1102a7c454d051e00e7bd249c",
+    "url": "/portfolio/static/media/Ravenous.90affcd1.PNG"
+  },
+  {
+    "revision": "1cf68f6d83feac9d82f18397c6940425",
+    "url": "/portfolio/static/media/ResReview.1cf68f6d.PNG"
+  },
+  {
+    "revision": "400eee4c02214625db4bfaca0851ab11",
+    "url": "/portfolio/static/media/Rock.400eee4c.PNG"
+  }
+]);
